8d8ce3a0064e489cdabc9836124fb0a62997c826
ainanicolau <ainaa.nic@gmail.com>
2023-08-04 20:22:32 +0200

    Add images for odd days

--- src/Day.js
+++ src/Day.js
@@ -3,23 +3,31 @@ import Task from './Task';
 
 function Day({ dayIndex, tasks, updateTasks }) {
   const allTasksCompleted = tasks.every(task => task.completed);
+  const isOddRow = dayIndex % 2 == 0;
 
   return (
-    <div className={`day ${allTasksCompleted ? 'completed' : ''}`}>
-      <div className="day-content">
-        <h2>Dia {dayIndex + 1}</h2>
-        <div className="tasks">
-          {tasks.map((task, taskIndex) => (
-            <Task
-              key={taskIndex}
-              dayIndex={dayIndex}
-              taskIndex={taskIndex}
-              task={task}
-              updateTasks={updateTasks}
-            />
-          ))}
+    <div className="row">
+      <div className={`day ${allTasksCompleted ? 'completed' : ''} ${isOddRow ? '' : 'isEvenRow'}`}>
+        <div className="day-content">
+          <h2>Dia {dayIndex + 1}</h2>
+          <div className="tasks">
+            {tasks.map((task, taskIndex) => (
+              <Task
+                key={taskIndex}
+                dayIndex={dayIndex}
+                taskIndex={taskIndex}
+                task={task}
+                updateTasks={updateTasks}
+              />
+            ))}
+          </div>
         </div>
       </div>
+      {isOddRow && (
+        <div className="image-container">
+          <img src="logo192.png" alt="Image" />
+        </div>
+      )}
     </div>
   );
 }
